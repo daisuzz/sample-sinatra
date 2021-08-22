bb3dc860637f7670f547b83ae8481f1c64569672
daisuzz <daisaku.rugby@gmail.com>
2021-08-22 13:58:01 +0900

    add sample api

--- Gemfile
+++ Gemfile
@@ -4,9 +4,7 @@ source 'https://rubygems.org'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
-
+gem 'faraday'
 gem 'sinatra', '~> 2.1'
-
-gem 'thin', '~> 1.8'
-
 gem 'sinatra-contrib', '~> 2.1'
+gem 'thin', '~> 1.8'
--- app.rb
+++ app.rb
@@ -1,14 +1,34 @@
+require 'faraday'
 require 'sinatra'
+require 'json'
+require 'sinatra/json'
 require 'sinatra/reloader'
 
-get '/hello/:name' do |name|
-  "hello #{name}"
+# change a base folder for static file
+set public_folder: "#{__dir__}/static"
+
+field_list = %w[userId id title body]
+
+get '/' do
+  redirect '/index.html'
 end
 
-get '/goodbye' do 
-  "good bye #{params[:name]}"
+get '/posts' do
+  field = params.fetch(:field)
+
+  unless field_list.include?(field)
+    status 400
+    body 'field not found!'
+    return
+  end
+
+  posts = JSON.parse(Faraday.get('https://jsonplaceholder.typicode.com/posts').body)
+
+  res = posts.map { |post| { "#{field}": post[field].to_s } }
+
+  json res
 end
 
-get '/erb_template_page' do
-  erb :erb_template_page
+get '/posts/:id' do
+  Faraday.get("https://jsonplaceholder.typicode.com/posts/#{params[:id]}").body
 end
